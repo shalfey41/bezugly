db14c4ac70c1e91252d48b3bfcf3bea5a48a40c7
shalfey41 <bezu-dima2250@yandex.ru>
2021-03-13 15:29:11 +0300

    Причесал конфиг и создал страницы сайта

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
+import { Fragment } from 'react';
+import Head from 'next/head'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Fragment>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Component {...pageProps} />
+    </Fragment>
+  );
 }
 
 export default MyApp
