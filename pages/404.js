89395a1c32539f58133375645778cd1323056295
shalfey41 <bezu-dima2250@yandex.ru>
2021-03-21 01:32:50 +0300

    Стилизовал 404 страницу

--- pages/404.js
+++ pages/404.js
@@ -1,3 +1,49 @@
+import Link from "next/link";
+import Head from "next/head";
+
+import theme from "../styles/theme.module.css";
+import style from "../styles/main-page.module.css";
+import { NavList } from "../components/NavList/NavList";
+
 export default function Custom404() {
-  return <h1>404 - Page Not Found</h1>
+  return (
+    <div className={`${theme.page} ${style.page}`}>
+      <Head>
+        <title>Страница не найдена — Дима Безуглый</title>
+        <meta property="og:title" content="Страница не найдена — Дима Безуглый" />
+        <meta property="og:site_name" content="Страница не найдена — Дима Безуглый" />
+        <meta name="twitter:title" content="Страница не найдена — Дима Безуглый" />
+        <meta property="og:description" content="Но когда-нибудь она обязательно найдется" />
+        <meta name="twitter:description" content="Но когда-нибудь она обязательно найдется" />
+        <meta property="og:image" content="https://bezugly.ru/images/main-page-avatar.png" />
+        <meta property="twitter:image:src" content="https://bezugly.ru/images/main-page-avatar.png" />
+      </Head>
+
+      <header className={theme.header}>
+        <div className={`${theme.pageContainer} ${style.headerInner}`}>
+          <NavList className={style.navList} />
+          <h1 className={`${theme.title} ${style.title}`}>Страница не найдена</h1>
+        </div>
+      </header>
+      <main>
+        <div className={theme.pageContainer}>
+          <div className={style.content}>
+            <Link href="/">
+              <img
+                className={style.avatar}
+                src="/images/main-page-avatar.png"
+                alt="Моя фотография"
+                width="142"
+                style={{ cursor: 'pointer' }}
+              />
+            </Link>
+            <div className={style.textColumn}>
+              <p className={`${theme.text} ${style.text}`}>Но когда-нибудь она обязательно найдется</p>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+
 }
