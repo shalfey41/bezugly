895bb8da2d731cf63534e74cac7e6d2e47b838b4
shalfey41 <bezu-dima2250@yandex.ru>
2022-12-28 01:43:29 +0700

    Выпилил Яндекс.Метрику

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,6 @@ import Head from 'next/head'
 import 'normalize.css'
 
 import '../styles/globals.css'
-import { isDev } from '../helpers/env'
 
 const App: FC<AppProps> = ({ Component, pageProps }) => {
   return (
@@ -22,23 +21,6 @@ const App: FC<AppProps> = ({ Component, pageProps }) => {
         <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#5bbad5" />
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#ffffff" />
-
-        {!isDev() && (
-          <Fragment>
-            {/* Yandex.Metrika counter */}
-            <meta name="yandex-verification" content="892f090ef843257d" />
-            <script
-              dangerouslySetInnerHTML={{
-                __html: `(function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter31371433 = new Ya.Metrika2({ id:31371433, clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true, trackHash:true }); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = "https://mc.yandex.ru/metrika/tag.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks2");`,
-              }}
-            />
-            <noscript
-              dangerouslySetInnerHTML={{
-                __html: `<div><img src="https://mc.yandex.ru/watch/31371433" style="position:absolute; left:-9999px;" alt="" /></div>`,
-              }}
-            />
-          </Fragment>
-        )}
       </Head>
 
       <Component {...pageProps} />
